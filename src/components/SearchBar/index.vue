ded34b1460fb988ea90c77a9b86c3b237bdde34b
summe-debug <1113708534@qq.com>
2021-07-13 16:28:58 +0800

    'v0.1'

---
+++ src/components/SearchBar/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="search">
+    <el-input @focus="focus" @blur="blur" @keyup.enter.native="searchHandler">
+      <i slot="suffix" class="el-input__icon el-icon-search"></i>
+    </el-input>
+    <el-card v-if="isSearch">...</el-card>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "SearchBar",
+};
+</script>
---
+++ src/views/Search/Search.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <MainLayout>
+      <div class="search-wrapper">
+        <img class="logo" src="~@/assets/img/giligili_white.png" />
+        <SearchBar class="search-bar" />
+      </div>
+    </MainLayout>
+  </div>
+</template>
+
+<script>
+import SearchBar from "@/components/SearchBar";
+import MainLayout from "@/components/MainLayout";
+export default {
+  name: "Search",
+  components: {
+    SearchBar,
+    MainLayout,
+  },
+};
+</script>
+
+<style scoped>
+.search-wrapper {
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  background-color:red;
+}
+.logo {
+  margin-left: 40px;
+  width: 100px;
+}
+.search-bar {
+  margin-left: 20px;
+}
+</style>
