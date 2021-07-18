ec73564a12afce1ceef3e4df2b8a4f743aec3e9c
unknown <1156609896@qq.com>
2021-07-18 16:36:18 +0800

    v0.5

---
+++ src/views/Position/Position.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <div>123</div>
+  </div>
+</template>
+<script>
+export default {
+  props: {
+    item: {},
+  },
+  data() {
+    return {};
+  },
+};
+</script>
+<style scoped></style>
